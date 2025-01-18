36411d5879e2232cea08b83d3b3c1dc5d888f77d
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-01-18 09:10:39 +0600

    Complete showing static movie with card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Result from "./component/Result";
-
+import axios from "axios";
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 function App() {
-  const [movies, setMovies] = useState([1, 2, 3]);
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
+  const getAllMovies = () => {
+    axios
+      .get(APIURL)
+      .then((response) => setMovies(response.data.results))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (search === "") {
+      getAllMovies();
+    } else {
+      getSearchMovies();
+    }
+  }, []);
   return (
     <>
       <div className="max-w-[1240px] shadow-xl min-h-[400px] mx-auto p-3">
@@ -13,7 +34,7 @@ function App() {
           type="search"
           // value={search}
           // onChange={changeTheSearch}
-          className="w-full border border-black rounded text-slate-700 p-4"
+          className="w-full border hover:border-none border-black rounded text-slate-800 p-4"
         />
         <Result movies={movies} />
       </div>
--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -2,17 +2,35 @@ import React from "react";
 
 const Result = ({ movies }) => {
   const boxes = movies.map((item, index) => {
-    return <Box />;
+    return (
+      <Box
+        key={index}
+        image={item.poster_path}
+        title={item.original_title}
+        rating={item.vote_average}
+      />
+    );
   });
   return <div className="w-full grid md:grid-cols-4 gap-5">{boxes}</div>;
 };
 
 export default Result;
 
-const Box = () => {
+const Box = (props) => {
+  const IMGPATH = "https://image.tmdb.org/t/p/w1280";
   return (
-    <div className="shadow min-h-[200px] mt-3 pb-1 border">
-      <p>hello</p>
+    <div className="shadow min-h-[220px] mt-4 pb-1 m-3 hover:shadow-lg transform transition duration-300 hover:scale-105 rounded">
+      <img src={IMGPATH + props.image} alt="" className="w-full p-2 rounded" />
+      <div className="px-5 my-3 text-left">
+        <p className="text-xl">
+          <span className="font-semibold"> Name</span> :{" "}
+          <span className="font-normal">{props.title}</span>
+        </p>
+        <p className="text-xl font-semibold">
+          Rating :{" "}
+          <span className="text-orange-700 font-normal">{props.rating}</span>
+        </p>
+      </div>
     </div>
   );
 };
